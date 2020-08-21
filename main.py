88a08ddba19026c87ecdd3685ff4a9690f81fea8
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-08-20 23:03:09 -0500

    initial imports and defs in main.py

--- main.py
+++ main.py
@@ -5,6 +5,37 @@ import psycopg2
 
 app = Flask(__name__)
 api = Api(app)
+# assign database name & location to variable "db"
+db = "dbname=%s host=%s " % ('mini_stats', 'localhost')
+schema = "schema.sql"
+# initializes connection to PostgreSQL
+def get_connection():
+  connection = psycopg2.connect(db)
+  return connection
+
+# closes connection to PostgreSQL
+def close_connection(connection):
+  if connection:
+    connection.close()
+    print("Postgres connection is now closed")
+
+# prints database version
+def read_database_version():
+  try:
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT version();")
+    db_version = cursor.fetchone()
+    print("You are connect to PostgreSQL version: ", db_version)
+    close_connection(connection)
+  except (Exception, psycopg2.Error) as error:
+    print("Error while getting data", error)
+
+# runs read_database_version and tests connection to PostgreSQL
+print("Printing Database version:")
+read_database_version()
+
+
 
 class Frosty(Resource):
   def get(self):
--- readme.md
+++ readme.md
@@ -1,9 +1,15 @@
 # Frosty stats project
+__By Nick Simonelli & Ryan Osgar__
+>Weekend Effort
 
 ## Description
-Web based application for submitting and displaying stats. Developed using a combination of python, react, and postgres
+Web based application for reporting, displaying, and analyzing stats for local Minneapolis sports club. Developed using a combination of python, javaScript, and SQL
 
 ## Installation
 
 ## Useage
+
+## Wireframe
+
+## Notes
 
